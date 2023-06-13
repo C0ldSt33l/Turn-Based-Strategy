441e11299c3ef7b397ac54b702b673c15b647c8c
C0ldSt33l <sychev2003di@gmail.com>
2023-06-13 23:46:05 +0300

    implement class Aoe_DMG_Dealer

--- Aoe_DMG_Dealer.cpp
+++ Aoe_DMG_Dealer.cpp
@@ -1,4 +1,5 @@
 #include "Aoe_DMG_Dealer.h"
+#include "Manager.h"
 
 
 Aoe_DMG_Dealer::Aoe_DMG_Dealer(Cell* cell, std::list<Unit*>* targets) : 
@@ -6,5 +7,21 @@ Aoe_DMG_Dealer::Aoe_DMG_Dealer(Cell* cell, std::list<Unit*>* targets) :
 Aoe_DMG_Dealer::~Aoe_DMG_Dealer() {}
 
 void Aoe_DMG_Dealer::action(sf::Vector2i const& point) {
+    if (!this->has_action_point) return;
 
+    Message* msg;
+    for (auto cell : this->attack_zone.get_zone()) {
+        if (cell && cell->unit) {
+            msg = new Message;
+            msg->sender = this;
+            msg->set_deal_dmg(this, cell->unit, this->damage);
+            Manager::get_instance().send_messange(msg);
+        }
+    }
+
+    this->has_action_point = false;
+    for (auto cell : this->attack_zone.get_zone()) {
+        if (!cell) continue;
+        cell->set_color(CELL_FILL_COLOR);
+    }
 }
